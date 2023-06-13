3e1c20928f0b2f8cf66ee69c83027dbde9799740
SamanSah <sahraei.saman91@gmail.com>
2023-06-13 20:36:59 +0330

    first commit test

---
+++ test.js
@@ -0,0 +1,11 @@
+console.clear();
+const log=console.log
+// global.setTimeout(()=> {
+//     log('helo');
+// }, 2000)
+
+// log(process)
+log(process.version)
+
+log(process.exit())
+log(process.arch)
