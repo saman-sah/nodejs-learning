474745718609b66e8bef62549ce83919f0d0958d
SamanSah <sahraei.saman91@gmail.com>
2023-06-14 23:11:44 +0330

    training os-path

---
+++ os-path/os.js
@@ -0,0 +1,13 @@
+const os = require('os');
+
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.version());
+console.log(os.type());
+console.log(os.hostname());
+console.log(os.release());
+console.log(os.cpus());
+console.log(os.uptime());
+console.log(os.loadavg());
+console.log(os.freemem());
+console.log(os.totalmem());
