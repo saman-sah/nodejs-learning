7f6807f92d9fe5fb1bf8c239ae362a5cc84af9a0
SamanSah <sahraei.saman91@gmail.com>
2023-06-21 12:29:17 +0330

    chalk, dotenv

---
+++ dotenv.js
@@ -0,0 +1,13 @@
+console.clear();
+import dotenv from 'dotenv'
+dotenv.config();
+
+import "dotenv/config"
+
+console.log(process.env.NUMBER);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+console.log(process.env.TEST1);
+console.log(process.env.TEST2);
+console.log(process.env.MULTILINE1);
+console.log(process.env.MULTILINE2);
